f22097e0c79cf985de4964157f3b1027b09c3b92
fsb3rke <berkeeavcii@gmail.com>
2024-01-07 11:57:41 +0300

    feat: add cmake integration

--- build.bat
+++
@@ -1,5 +0,0 @@
-if NOT EXIST build/ (
-	mkdir build
-)
-
-g++ -Wall -Werror -Wpedantic src/main.cpp src/fs.cpp src/CommandHandlerer.cpp src/cc.cpp -std=c++17 -Ilib -o build/main.exe
--- build.sh
+++
@@ -1,6 +0,0 @@
-BUILD_DIR="./build"
-if [ ! -d "$BUILD_DIR" ]; then
-	mkdir "$BUILD_DIR"
-fi
-
-g++ -Wall -Werror -Wpedantic src/main.cpp src/fs.cpp src/CommandHandlerer.cpp src/cc.cpp -std=c++17 -Ilib -o build/main.obj
